a6354791aa4703cc23b084a0d4e34738206460e5
Scottiedog45 <scottiedog45@gmail.com>
2017-12-05 22:55:52 -0500

    test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const faker = require('faker');
 const mongoose = require('mongoose');
 const should = chai.should();
 const {Haiku} = require('../models');
