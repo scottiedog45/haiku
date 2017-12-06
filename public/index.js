5096d4f31e2b2aad386fe421d591703eba26ba95
Scottiedog45 <scottiedog45@gmail.com>
2017-12-06 13:31:11 -0500

    most of the way there

--- public/index.js
+++ public/index.js
@@ -1,12 +1,3 @@
-//make the api call for each word in the haiku,
-//return the syllable count and to make sure it's a word BOOLEAN
-
-//can this be called everytime the user enters a space? 
-//parse the line into words, indicees of an array
-//push the results of the api call into a new array let syllableCountLineOne[], lineTwo[], lineThree[]
-// they don't equal 5/7/5, log number, and ask user to repoem
-//once completed, show a post poem button, 
-//post poem to mongo db, edit and delete functionality
 var syllablesFirstLine = 0;
 var syllablesSecondLine = 0;
 var syllablesThirdLine = 0;
@@ -71,6 +62,7 @@ function getFirstLineSyllables(word) {
 	}).done(function(a){
 		console.log(a);
 		syllablesFirstLine += a.syllables.count;
+		console.log(syllablesFirstLine);
 	});
 }
 
@@ -104,20 +96,125 @@ function getThirdLineSyllables(word) {
 
 function checkFirstLine(line) {
 	if (line = 5) {
+		alert('syllables achieved. compose second line');
 		$('#secondLine').prop('disabled',false);
 	}
 }
 
 function checkSecondLine(line) {
 	if (line = 7) {
+		alert('syllables achieved, compose third line')
 		$('#thirdLine').prop('disabled',false);
 	}
 }
 
 function checkThirdLine(line) {
 	if (line = 5) {
-		alert('you win!');
+		alert('syllables achieved. you win');
+		$('#compositionForm').append(`
+			<button id='post' type='post'>send this haiku to the masses</button>`);
 	}
 }
 
+$(document).ready(getHaikus());
 
+function getHaikus() {
+	$.ajax({
+		type:'GET',
+		datatype: 'json',
+		url:'/haikus',
+		success: function(data) {
+			displayGottenHaikus(data);
+		}
+	});
+}
+
+function displayGottenHaikus(data) {
+	console.log(data);
+		for (i=0;i<data.length;i++) {
+			$('#log').append(`
+				<div class='wholeShebang'>
+				<p class='postedTitle'>title: ${data[i].title}</p>
+				<p class='postedAuthor'>author: ${data[i].author}</p>
+				<p class='postedHaikuLineOne'>${data[i].lines.lineOne}</p>
+				<p class='postedHaikuLineTwo'>${data[i].lines.lineTwo}</p>
+				<p class='postedHaikuLineThree'${data[i].lines.lineThree}</p>
+				<p class='votes'></p>
+				<button class='delete' data-id='${data[i].id}'>trash this haiku</button>
+				<button class='vote' data-votes=0>vote up</button>
+				<button class='edit'>edit</button>
+				</div>
+				`);}
+}
+
+$('#log').on('click', '.vote', function(event) {
+	event.preventDefault();
+	let votes = $(this).data('votes');
+	console.log(votes);
+	votes += 1;
+	console.log(votes);
+	$(this).data('votes', votes);
+	$(this).siblings('.votes').html(votes);
+})
+
+$('#compositionForm').on('click', '#post', function(event) {
+	event.preventDefault();
+	let title = $(this).parent().find('#title').val();
+	let author = $(this).parent().find('#author').val();
+	let lineOne = $(this).parent().find('#firstLine').val();
+	let lineTwo = $(this).parent().find('#secondLine').val();
+	let lineThree = $(this).parent().find('#thirdLine').val();
+	let datas = {
+		"title": title,
+		"author": author,
+		"lines": {
+			"lineOne": lineOne,
+			"lineTwo": lineTwo,
+			"lineThree": lineThree
+		}
+	}
+	$.ajax({
+		url:'/haikus',
+		type: 'POST',
+    contentType: 'application/json; charset=utf-8',
+    data: JSON.stringify(datas),
+	});
+	getHaikus();
+});
+
+$('#log').on('click', '.delete', function(event) {
+	event.preventDefault();
+	let id = $(this).data('id');
+	$(this).closest('.wholeShebang').remove();
+	$.ajax({
+		method: 'delete',
+		url: '/haikus/' + id,
+	});
+});
+
+$('#log').on('click', '.edit', function(event){
+	event.preventDefault();
+	let id = $(this).data('id');
+	let title = $(this).parent().find('.postedTitle').text();
+	let author = $(this).parent().find('.postedAuthor').val();
+	let firstLine = $(this).parent().find('.postedHaikuLineOne').val();
+	let secondLine = $(this).parent().find('.postedHaikuLineTwo').val();
+	let thirdLine = $(this).parent().find('.postedHaikuLineThree').val();
+	$(this).closest('.wholeShebang').html(`
+		<input class='title' value='${title}'>
+		<button type='put'>update this haiku</button>
+		`)
+
+})
+
+// $('#postHaiku').on('click', function() {
+// 	$.ajax({
+// 		method: 'POST',
+// 		url: 
+// 	});
+// })
+
+
+
+// $('#editHaiku').on('click', function {
+// })
