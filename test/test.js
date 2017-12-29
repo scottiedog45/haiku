c4a2e6025d00ef73b390d8b207e535aeb1a65d8a
Scottiedog45 <scottiedog45@gmail.com>
2017-12-29 16:48:47 -0500

    yes

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const should = chai.should();
 const {Haiku} = require('../models');
 const {app, runServer, closeServer} = require('../server');
-const {DATABASE_URL} = require('../config');
+const {TEST_DATABASE_URL} = require('../config');
 
 var server = require('../server.js')
 var storage = server.storage;
@@ -74,7 +74,7 @@ function tearDownDb() {
 
 describe('haiku API resource', function() {
 	before(function() {
-		return runServer(DATABASE_URL);
+		return runServer(TEST_DATABASE_URL);
 	});
 
 	beforeEach(function(){
@@ -217,6 +217,3 @@ describe('haiku API resource', function() {
 		});
 	});
 });
-
-
-
